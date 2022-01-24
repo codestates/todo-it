5795ad52b6d101ae3e23bfe902781e182f05502b
alsqja <alsqha2626@gmail.com>
2022-01-24 16:56:22 +0900

    프로필 페이지 수정

--- client/src/pages/ProfilePage/Profile.tsx
+++ client/src/pages/ProfilePage/Profile.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import Header from "../../components/Header/Header";
 import {
   StyledDiv,
   KeyInput,
@@ -35,7 +34,6 @@ export const Profile = () => {
     <UserSettingPageContainer>
       {isLogin ? <UserLoginModal LoginModalHandler={LoginModalHandler}/> : ''}
       {isWithdrawal ? <WithdrawalModal WithdrawalModalHandler={WithdrawalModalHandler}/> : ''}
-      <Header/>
       <Body>
         <StyledDiv>
           <KeyInput>닉네임 :</KeyInput>
