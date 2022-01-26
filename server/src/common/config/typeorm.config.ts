f4287ee3229d0149d11eeec9e21e2eaaadf53887
Nix <72999818+nix6839@users.noreply.github.com>
2022-01-26 09:14:37 +0900

    [Server] Refactor about configService (#72)
    * [Server] Remove unnecessary configService infer
    
    * [Server] Use configService instead of process.env

--- server/src/common/config/typeorm.config.ts
+++ server/src/common/config/typeorm.config.ts
@@ -14,11 +14,11 @@ export const typeOrmModuleOptions: TypeOrmModuleAsyncOptions = {
     configService: ConfigService<EnvironmentVariables, true>
   ): TypeOrmModuleOptions => ({
     type: 'mariadb',
-    host: configService.get('DB_HOST', { infer: true }),
-    port: configService.get('DB_PORT', { infer: true }),
-    username: configService.get('DB_USERNAME', { infer: true }),
-    password: configService.get('DB_PASSWORD', { infer: true }),
-    database: configService.get('DB_NAME', { infer: true }),
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
     entities: [User, Todo],
     // TODO: 밑의 옵션 두개는 개발모드에서만 적용
     synchronize: true,
