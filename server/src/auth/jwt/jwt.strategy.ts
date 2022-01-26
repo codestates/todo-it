4fa6db7d31aa63bc2a86b8d86c6abc0cff90a313
Nix <nix6839@washnix.com>
2022-01-26 09:11:32 +0900

    [Server] Use configService instead of process.env

--- server/src/auth/jwt/jwt.strategy.ts
+++ server/src/auth/jwt/jwt.strategy.ts
@@ -1,15 +1,20 @@
+import { ConfigService } from '@nestjs/config';
 import { JwtPayload } from './jwt.payload';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { EnvironmentVariables } from 'src/env';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
-    const superArg: StrategyOptions = {
+  constructor(
+    readonly configService: ConfigService<EnvironmentVariables, true>
+  ) {
+    super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
-    };
-    super(superArg);
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
   }
 
   validate(payload: JwtPayload) {
