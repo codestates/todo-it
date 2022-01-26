47281b8696447fd6d32d3878afb3cf6848a41703
Nix <72999818+nix6839@users.noreply.github.com>
2022-01-27 04:05:00 +0900

    [Server] Update set directory id when add todo (#103)

--- server/src/todos/todos.module.ts
+++ server/src/todos/todos.module.ts
@@ -1,3 +1,4 @@
+import { Directory } from '../directories/entities/directory.entity';
 import { UserRepository } from '../users/repositories/user.repository';
 import { Todo } from 'src/todos/entities/todo.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -5,7 +6,7 @@ import { Module } from '@nestjs/common';
 import { TodosService } from './services/todos.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Todo, UserRepository])],
+  imports: [TypeOrmModule.forFeature([Todo, UserRepository, Directory])],
   providers: [TodosService],
   exports: [TodosService],
 })
