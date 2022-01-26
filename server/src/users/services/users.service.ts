59f20ac1d4a4791f52263474a7f813ef62ce1795
Nix <72999818+nix6839@users.noreply.github.com>
2022-01-26 15:03:26 +0900

    [Server] Add add user todo api (#78)

--- server/src/users/services/users.service.ts
+++ server/src/users/services/users.service.ts
@@ -6,9 +6,9 @@ import {
   ForbiddenException,
 } from '@nestjs/common';
 import { UserRegisterDto } from '../dto/user-register.dto';
-import { generateRandomNickname } from 'src/common/utils/generateRandomNickname';
-import { User } from '../entities/user.entity';
-import { Todo } from 'src/todos/entities/todo.entity';
+import { generateRandomNickname } from 'src/common/utils/gen-random-nick.util';
+import { pickTodoData } from 'src/common/utils/pick-todo-data.util';
+import { pickUserData } from 'src/common/utils/pick-user-data.util';
 
 @Injectable()
 export class UsersService {
@@ -31,12 +31,12 @@ export class UsersService {
       password,
       nickname,
     });
-    return this.pickUserData(newUser);
+    return pickUserData(newUser);
   }
 
   async getUserById(userId: number) {
     const user = await this.userRepository.findOneOrFail(userId);
-    return this.pickUserData(user);
+    return pickUserData(user);
   }
 
   async updateUserById(
@@ -53,7 +53,7 @@ export class UsersService {
     if (nickname !== undefined) {
       user.nickname = nickname;
     }
-    return this.pickUserData(await user.save());
+    return pickUserData(await user.save());
   }
 
   async deleteUserById(userId: number) {
@@ -65,7 +65,7 @@ export class UsersService {
     const user = await this.userRepository.findOneOrFail(userId, {
       relations: ['todos'],
     });
-    return user.todos.map(this.pickTodoData);
+    return user.todos.map(pickTodoData);
   }
 
   private async generateNoDuplicateNickname(): Promise<string> {
@@ -74,23 +74,5 @@ export class UsersService {
       nickname = generateRandomNickname();
     } while (await this.userRepository.exists({ nickname }));
     return nickname;
-  }
-
-  private pickUserData(user: User) {
-    return {
-      id: user.id,
-      email: user.email,
-      nickname: user.nickname,
-    };
-  }
-
-  private pickTodoData(todo: Todo) {
-    return {
-      id: todo.id,
-      content: todo.content,
-      isDone: todo.isDone,
-      deadline: todo.deadline,
-      comment: todo.comment,
-    };
   }
 }
--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,3 +1,5 @@
+import { TodosModule } from '../todos/todos.module';
+import { UserMeTodosController } from './controllers/user-me-todos.controller';
 import { UsersMeController } from './controllers/users-me.controller';
 import { UserRepository } from './repositories/user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,8 +8,8 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
-  controllers: [UsersMeController, UsersController],
+  imports: [TypeOrmModule.forFeature([UserRepository]), TodosModule],
+  controllers: [UsersMeController, UserMeTodosController, UsersController],
   providers: [UsersService],
 })
 export class UsersModule {}
