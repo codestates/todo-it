aff20dbc5708645eddff1484b600fd07c381e7b0
alsqja <alsqha2626@gmail.com>
2022-01-26 23:15:04 +0900

    [Client] 로그인 요청 accessToken 수정, 회원 탈퇴 요청, 유저정보 수정 요청 구현

--- client/src/pages/UserInfoSettingPage/UserInfoSettingPage.tsx
+++ client/src/pages/UserInfoSettingPage/UserInfoSettingPage.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { 
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import {
   Body,
   StyledDiv,
   KeyInput,
@@ -8,80 +8,85 @@ import {
   ValueInput,
   Warning,
   ButtonBox,
-  StyledButton
- } from "../SignupPage/SignupPage";
+  StyledButton,
+} from '../SignupPage/SignupPage';
+import axios from 'axios';
 
-const UserInfoSettingPageContainer = styled.div`
-  
-`
+const UserInfoSettingPageContainer = styled.div``;
 
 export const UserInfoSettingPage = () => {
-  const [name, setName] = useState('')
-  const [isName, setIsName] = useState(true)
-  const [email, setEmail] = useState('')
-  const [isEmail, setIsEmail] = useState(true)
-  const [password, setPassword] = useState('')
-  const [isPass, setIsPass] = useState(true)
-  const [checkPass, setCheckPass] = useState('')
-  const [isCheck, setIsCheck] = useState(true)
+  const [name, setName] = useState('');
+  const [isName, setIsName] = useState(true);
+  const [newpassword, setNewPassword] = useState('');
+  const [isPass, setIsPass] = useState(true);
+  const [checkPass, setCheckPass] = useState('');
+  const [isCheck, setIsCheck] = useState(true);
+
+  const [originPassword, setOriginPassword] = useState('');
 
   const NameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value)
-    const CheckId = (name: string):boolean => {
-      return (name.length <= 14 && name.length >= 4) || name.length === 0
-    }
+    setName(event.target.value);
+    const CheckId = (name: string): boolean => {
+      return (name.length <= 14 && name.length >= 4) || name.length === 0;
+    };
     if (!CheckId(event.target.value)) {
-      setIsName(false)
+      setIsName(false);
+    } else {
+      setIsName(true);
     }
-    else {
-      setIsName(true)
-    }
-  }
-
-  const EmailHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value)
-    const CheckEmail = (email: string):boolean => {
-      return email.includes('@') || email.length === 0
-    }
-    if (!CheckEmail(event.target.value)) {
-      setIsEmail(false)
-    }
-    else {
-      setIsEmail(true)
-    }
-  }
+  };
 
   const PasswordHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value)
-    const CheckPass = (password: string):boolean => {
-      return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,128}$/.test(password) || password.length === 0
-    }
+    setNewPassword(event.target.value);
+    const CheckPass = (password: string): boolean => {
+      return (
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,128}$/.test(
+          password
+        ) || password.length === 0
+      );
+    };
     if (!CheckPass(event.target.value)) {
-      setIsPass(false)
+      setIsPass(false);
+    } else {
+      setIsPass(true);
     }
-    else {
-      setIsPass(true)
-    }
-  }
+  };
 
   const CheckPassHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCheckPass(event.target.value)
-    if (password === event.target.value || event.target.value.length === 0) {
-      setIsCheck(true)
+    setCheckPass(event.target.value);
+    if (newpassword === event.target.value || event.target.value.length === 0) {
+      setIsCheck(true);
+    } else {
+      setIsCheck(false);
     }
-    else {
-      setIsCheck(false)
-    }
-  }
+  };
 
-  const SignupClickHandler = () => {
-    // TODO : signup 요청
-    // if (200 ok) => window.location.href = localhost:3000/login
-  }
+  const OriginPassHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setOriginPassword(event.target.value);
+  };
 
-  const verifyEmailHandler = () => {
-    // TODO : email 인증 요청
-  }
+  const UserInfoSettingHandler = () => {
+    axios
+      .patch(
+        'https://localhost:8000/users/me',
+        {
+          originalPassword: originPassword,
+          nickname: name,
+          newPassword: newpassword,
+        },
+        {
+          headers: { 'Content-Type': 'application/json' },
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        window.location.href = 'https://localhost:3000/';
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <UserInfoSettingPageContainer>
@@ -89,32 +94,71 @@ export const UserInfoSettingPage = () => {
         <StyledDiv>
           <KeyInput>닉네임 :</KeyInput>
           <InputBox>
-            <ValueInput type="text" placeholder="user.name" value={name} onChange={NameHandler}/>
-            <Warning style={isName ? {display: "none"} : {}}>닉네임은 4~14글자 입니다.</Warning>
+            <ValueInput
+              type="text"
+              placeholder="user.name"
+              value={name}
+              onChange={NameHandler}
+            />
+            <Warning style={isName ? { display: 'none' } : {}}>
+              닉네임은 4~14글자 입니다.
+            </Warning>
           </InputBox>
         </StyledDiv>
         <StyledDiv>
           <KeyInput>이메일 :</KeyInput>
-          <KeyInput style={{textAlign:"center", paddingLeft: "0", paddingRight:"160px"}}>user.email</KeyInput>
+          <KeyInput
+            style={{
+              textAlign: 'center',
+              paddingLeft: '0',
+              paddingRight: '160px',
+            }}
+          >
+            user.email
+          </KeyInput>
+        </StyledDiv>
+        <StyledDiv>
+          <KeyInput>기존 비밀번호 :</KeyInput>
+          <InputBox>
+            <ValueInput
+              type="password"
+              value={originPassword}
+              onChange={OriginPassHandler}
+            />
+          </InputBox>
         </StyledDiv>
         <StyledDiv>
           <KeyInput>비밀번호 :</KeyInput>
           <InputBox>
-            <ValueInput type="password" value={password} onChange={PasswordHandler}/>
-            <Warning style={isPass ? {display: "none"} : {}}>8 ~ 128자의 영문 대소문자, 숫자, 특수문자만 사용 가능합니다.</Warning>
+            <ValueInput
+              type="password"
+              value={newpassword}
+              onChange={PasswordHandler}
+            />
+            <Warning style={isPass ? { display: 'none' } : {}}>
+              8 ~ 128자의 영문 대소문자, 숫자, 특수문자만 사용 가능합니다.
+            </Warning>
           </InputBox>
         </StyledDiv>
         <StyledDiv>
           <KeyInput>비밀번호 확인 :</KeyInput>
           <InputBox>
-            <ValueInput type="password" value={checkPass} onChange={CheckPassHandler}/> 
-            <Warning style={isCheck ? {display:"none"} : {}}>비밀번호가 일치하지 않습니다.</Warning>
+            <ValueInput
+              type="password"
+              value={checkPass}
+              onChange={CheckPassHandler}
+            />
+            <Warning style={isCheck ? { display: 'none' } : {}}>
+              비밀번호가 일치하지 않습니다.
+            </Warning>
           </InputBox>
         </StyledDiv>
         <ButtonBox>
-          <StyledButton onClick={SignupClickHandler}>정보 수정</StyledButton>
+          <StyledButton onClick={UserInfoSettingHandler}>
+            정보 수정
+          </StyledButton>
         </ButtonBox>
       </Body>
     </UserInfoSettingPageContainer>
-  )
-}
+  );
+};
